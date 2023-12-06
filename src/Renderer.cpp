62169d304121e83d8779dddd5742b23fefa8c262
Alexandre <alexandre.beeckmans@student.howest.be>
2023-12-06 17:05:34 +0100

    W10_Delete all unnecessary functions and variables

--- src/Renderer.cpp
+++ src/Renderer.cpp
@@ -31,9 +31,6 @@ Renderer::Renderer(SDL_Window* pWindow) :
 	m_Camera.aspectRatio = static_cast<float>(m_Width) / m_Height;
 
 	//InitTexture
-	//m_pTexture = Texture::LoadFromFile("Resources/uv_grid_2.png");
-	/*m_pTexture = Texture::LoadFromFile("Resources/tuktuk.png");*/
-
 	m_pTexture = Texture::LoadFromFile("Resources/vehicle_diffuse.png");
 	m_pNormalMap = Texture::LoadFromFile("Resources/vehicle_normal.png");
 	m_pGlossyMap = Texture::LoadFromFile("Resources/vehicle_gloss.png");
@@ -76,17 +73,11 @@ void Renderer::Render()
 	SDL_LockSurface(m_pBackBuffer);
 
 	//Functions
-	//Render_W1_Part1(); //Rasterizer stage only
-	//Render_W1_Part2();
-	//Render_W1_Part3();
-	//Render_W1_Part4();
-	//Render_W1_Part5();
-	//Render_W2_Part1();
-	//Render_W2_Part2();
-	//Render_W2_Part3();
-	//Render_W3_Part1();
-	//Render_W3_Part2();
-	//Render_W4_Part1();
+	//All the functions from the previous weeks were deleted to keep the code cleaner
+	//You can find them on my GitHub page :
+	//https://github.com/AlexandreBeeckmans/Rasterizer
+
+	Render_W4_Part1();
 
 	//@END
 	//Update SDL Surface
@@ -116,31 +107,6 @@ void Renderer::VertexTransformationFunction(std::vector<Mesh>& meshVector) const
 			newViewDirection.Normalize();
 			mesh.vertices_out.emplace_back(Vertex_Out{ newPos, vertex.color, vertex.uv, newNormal, newTangent, newViewDirection });
 		}
-	}
-}
-void Renderer::VertexTransformationFunction(const std::vector<Vertex>& vertices_in, std::vector<Vertex>& vertices_out) const
-{
-	//Todo > W1 Projection Stage
-	const float aspectRatio{ static_cast<float>(m_Width) / m_Height };
-	for (const Vertex& v : vertices_in)
-	{
-		
-		const Vector3 cameraToVertex{ m_Camera.origin, v.position };
-
-		Vertex viewVertex{ m_Camera.viewMatrix.TransformPoint(v.position)};
-
-		float projectedX{ viewVertex.position.x / (viewVertex.position.z) };
-		float projectedY{ viewVertex.position.y / (viewVertex.position.z) };
-
-		projectedX /= (m_Camera.fov * aspectRatio);
-		projectedY /= m_Camera.fov;
-
-		viewVertex.position.x = projectedX;
-		viewVertex.position.y = projectedY;
-
-		viewVertex.color = v.color;
-		viewVertex.uv = v.uv;
-		vertices_out.push_back(viewVertex);
 	}
 }
 
@@ -179,38 +145,6 @@ bool Renderer::SaveBufferToImage() const
 {
 	return SDL_SaveBMP(m_pBackBuffer, "Rasterizer_ColorBuffer.bmp");
 }
-
-bool Renderer::HitTest_Triangle(const std::vector<Vector2>& triangle, const Vector2& pixel, Vector3& barycentricWeights, const int triangleNr) const
-{
-
-
-	return HitTest_Triangle(triangle[triangleNr], triangle[triangleNr + 1], triangle[triangleNr + 2], pixel, barycentricWeights);
-
-	//const Vector2 v0{ triangle[triangleNr], pixel };
-	//const Vector2 v1{ triangle[triangleNr + 1], pixel };
-	//const Vector2 v2{ triangle[triangleNr + 2], pixel };
-
-	//const Vector2 s0{ triangle[triangleNr], triangle[triangleNr + 1] };
-	//const Vector2 s1{ triangle[triangleNr + 1], triangle[triangleNr + 2] };
-	//const Vector2 s2{ triangle[triangleNr + 2], triangle[triangleNr] };
-
-
-	//const float c0{ Vector2::Cross(s1, v1) };
-	//const float c1{ Vector2::Cross(s2, v2) };
-	//const float c2{ Vector2::Cross(s0, v0) };
-
-	//if ((c1 >= 0 && c2 >= 0 && c0 >= 0) || (c1 <= 0 && c2 <= 0 && c0 <= 0))
-	//{
-	//	const float triangleArea{ Vector2::Cross(s2, s0) };
-
-	//	barycentricWeights.x = c0 / triangleArea;
-	//	barycentricWeights.y = c1 / triangleArea;
-	//	barycentricWeights.z = c2 / triangleArea;
-
-	//	return true;
-	//}
-	//return false;
-}
 bool Renderer::HitTest_Triangle(const Vector2& vertex0, const Vector2& vertex1, const Vector2& vertex2, const Vector2& pixel, Vector3& barycentricWeights) const
 {
 	const Vector2 v0{ vertex0, pixel };
@@ -241,96 +175,6 @@ bool Renderer::HitTest_Triangle(const Vector2& vertex0, const Vector2& vertex1, 
 	return false;
 }
 
-void Renderer::RasterizeTriangle(const std::vector<Vertex>& vertices) const
-{
-	std::vector<Vector2> vetrices_screenSpace{};
-	float* depthBufferPixels{ new float[m_Width * m_Height] };
-	for (int i{ 0 }; i < m_Width * m_Height; ++i)
-	{
-		depthBufferPixels[i] = FLT_MAX;
-		m_pBackBufferPixels[i] = 0;
-	}
-
-	//Convert to ScreenSpace
-	for (const Vertex& v : vertices)
-	{
-		vetrices_screenSpace.push_back(ToScreenSpace(v.position.x, v.position.y));
-	}
-
-	for (int triangleNr{ 0 }; triangleNr < vertices.size(); triangleNr += 3)
-	{
-		//Define triangle bounding box
-		Vector2 topLeft
-		{
-			std::min<float>(vertices[triangleNr].position.x, std::min(vertices[triangleNr + 1].position.x, vertices[triangleNr + 2].position.x)),
-			std::max<float>(vertices[triangleNr].position.y, std::max(vertices[triangleNr + 1].position.y, vertices[triangleNr + 2].position.y))
-		};
-		ClampToNDC(topLeft);
-		topLeft = ToScreenSpace(topLeft.x, topLeft.y);
-
-		Vector2 bottomRight
-		{
-			std::max<float>(vertices[triangleNr].position.x, std::max(vertices[triangleNr + 1].position.x, vertices[triangleNr + 2].position.x)),
-			std::min<float>(vertices[triangleNr].position.y, std::min(vertices[triangleNr + 1].position.y, vertices[triangleNr + 2].position.y))
-		};
-		ClampToNDC(bottomRight);
-		bottomRight = ToScreenSpace(bottomRight.x, bottomRight.y);
-
-
-		//RENDER LOGIC
-		for (int px{int(topLeft.x)}; px < bottomRight.x; ++px)
-		{
-			for (int py{int(topLeft.y)}; py < bottomRight.y; ++py)
-			{
-				const Vector2 pixel
-				{
-					px + 0.5f,
-					py + 0.5f
-				};
-				const int pixelNr{ px + m_Width * py };
-				Vector3 barycentrics{};
-				if (HitTest_Triangle(vetrices_screenSpace, pixel, barycentrics, triangleNr))
-				{
-					//depthTest
-					const float w0{ barycentrics.x / vertices[triangleNr].position.z};
-					const float w1{ barycentrics.y / vertices[triangleNr + 1].position.z };
-					const float w2{ barycentrics.z / vertices[triangleNr + 2].position.z };
-
-					float zInterpolated{1 / (w0 + w1 + w2)};
-
-
-					
-					if (zInterpolated < depthBufferPixels[pixelNr])
-					{
-						depthBufferPixels[pixelNr] = zInterpolated;
-
-						//correctedUV
-						const Vector2 uv0{ barycentrics.x * (vertices[triangleNr].uv / vertices[triangleNr].position.z) };
-						const Vector2 uv1{ barycentrics.y * (vertices[triangleNr + 1].uv / vertices[triangleNr + 1].position.z) };
-						const Vector2 uv2{ barycentrics.z * (vertices[triangleNr + 2].uv / vertices[triangleNr + 2].position.z) };
-						const Vector2 interpolatedUV{ (uv0 + uv1 + uv2) * zInterpolated };
-
-						if (interpolatedUV.x >= 0 && interpolatedUV.x <= 1 && interpolatedUV.y >= 0 && interpolatedUV.y <= 1)
-						{
-							ColorRGB finalColor{};
-							finalColor = m_pTexture->Sample(interpolatedUV);
-
-							//Update Color in Buffer
-							finalColor.MaxToOne();
-
-							m_pBackBufferPixels[pixelNr] = SDL_MapRGB(m_pBackBuffer->format,
-								static_cast<uint8_t>(finalColor.r * 255),
-								static_cast<uint8_t>(finalColor.g * 255),
-								static_cast<uint8_t>(finalColor.b * 255));
-						}
-					}
-				}
-			}
-		}
-	}
-
-	delete[] depthBufferPixels;
-}
 void Renderer::RenderAPixel(const int px, const int py, const Vector4& v0, const Vector4& v1, const Vector4& v2, const Mesh& mesh, const int index)
 {
 	const int pixelNr{ px + (m_Width * py) };
@@ -525,51 +369,6 @@ Vector2 Renderer::ToScreenSpace(const float x, const float y) const
 	return{ newX, newY };
 }
 
-void Renderer::ClampToNDC(Vector2& point) const
-{
-	if (point.x > 1)
-	{
-		point.x = 1;
-	}
-	else if (point.x < -1)
-	{
-		point.x = -1;
-	}
-
-	if (point.y > 1)
-	{
-		point.y = 1;
-	}
-	else if (point.y < -1)
-	{
-		point.y = -1;
-	}
-}
-std::vector<Vertex> Renderer::MeshToVetrices(const Mesh& mesh) const
-{
-	int incrementIndex{1};
-	size_t maxSize{ mesh.indices.size() };
-
-	switch (mesh.primitiveTopology)
-	{
-		case PrimitiveTopology::TriangleList:
-			incrementIndex = 3;
-			break;
-		case PrimitiveTopology::TriangleStrip:
-			maxSize -= 2;
-			break;
-	}
-
-	std::vector<Vertex> vectorToFill{};
-	for (int currentIndice{ 0 }; currentIndice < maxSize; currentIndice += incrementIndex)
-	{
-
-		vectorToFill.push_back(mesh.vertices[mesh.indices[currentIndice]]);
-		vectorToFill.push_back(mesh.vertices[mesh.indices[currentIndice + 1]]);
-		vectorToFill.push_back(mesh.vertices[mesh.indices[currentIndice + 2]]);
-	}
-	return vectorToFill;
-}
 float Renderer::Remap(const float value, const float min, const float max) const
 {
 	if (max <= min || value > max || value < min) return 0;
@@ -623,8 +422,8 @@ ColorRGB Renderer::PixelShading(const Vertex_Out& vOut) const
 
 	float cosinus{ Vector3::Dot(reflect, vOut.viewDirection) };
 
-	//Return the value of cosinus to avoid big black zones
-	if (cosinus < 0) cosinus *= -1;
+	//Posiitve the value of cosinus to avoid big black zones
+	cosinus = std::abs(cosinus);
 
 	ColorRGB specularReflection
 	{ 
@@ -673,287 +472,6 @@ ColorRGB Renderer::PixelShading(const Vertex_Out& vOut) const
 	}
 
 	return{ 0,0,0 };
-}
-
-
-
-void Renderer::Render_W1_Part1() const
-{
-
-	//Define triangle
-	std::vector<Vector3> vetrices_ndc
-	{
-		{ 0.0f, 0.5f, 1.0f },
-		{ 0.5f, -0.5f, 1.0f },
-		{ -0.5f, -0.5f, 1.0f }
-	};
-
-
-	std::vector<Vector2> vetrices_screenSpace{};
-	//Convert to ScreenSpace
-	for (const Vector3& v : vetrices_ndc)
-	{
-
-		const float x = (v.x + 1) / 2 * m_Width;
-		const float y = (1 - v.y) / 2 * m_Height;
-
-		vetrices_screenSpace.push_back({ x, y });
-	}
-
-
-	//RENDER LOGIC
-	for (int px{}; px < m_Width; ++px)
-	{
-		for (int py{}; py < m_Height; ++py)
-		{
-			const Vector2 pixel
-			{
-				px + 0.5f,
-				py + 0.5f
-			};
-			ColorRGB finalColor{ 0, 0, 0 };
-			Vector3 barycentrics{};
-			if (HitTest_Triangle(vetrices_screenSpace, pixel, barycentrics, 0))
-			{
-				finalColor = { 1,1,1 };
-			}
-
-			//Update Color in Buffer
-			finalColor.MaxToOne();
-
-			m_pBackBufferPixels[px + (py * m_Width)] = SDL_MapRGB(m_pBackBuffer->format,
-				static_cast<uint8_t>(finalColor.r * 255),
-				static_cast<uint8_t>(finalColor.g * 255),
-				static_cast<uint8_t>(finalColor.b * 255));
-		}
-	}
-
-
-}
-void Renderer::Render_W1_Part2() const
-{
-
-	const std::vector<Vertex> vetrices_world
-	{
-		Vertex { { 0.0f, 2.0f, 0.0f } },
-		Vertex { { 1.0f, 0.0f, 0.0f } },
-		Vertex { { -1.0f, 0.0f, 0.0f} }
-	};
-
-	std::vector<Vertex> vetrices_NDC{};
-
-	VertexTransformationFunction(vetrices_world, vetrices_NDC);
-	RasterizeTriangle(vetrices_NDC);
-
-}
-void Renderer::Render_W1_Part3() const
-{
-	const std::vector<Vertex> vetrices_world
-	{
-		Vertex { { 0.0f, 2.0f, 0.0f }, { 1, 0, 0 } },
-		Vertex { { 1.0f, 0.0f, 0.0f }, { 0, 1, 0 } },
-		Vertex { { -1.0f, 0.0f, 0.0f}, { 0, 0, 1 } }
-	};
-
-	std::vector<Vertex> vetrices_NDC{};
-
-	VertexTransformationFunction(vetrices_world, vetrices_NDC);
-	
-	RasterizeTriangle(vetrices_NDC);
-
-
-}
-void Renderer::Render_W1_Part4() const
-{
-	const std::vector<Vertex> vetrices_world
-	{
-		//Triangle 0
-		Vertex { { 0.0f, 2.0f, 0.0f }, { 1, 0, 0 } },
-		Vertex { { 1.5f, -1.0f, 0.0f }, { 1, 0, 0 } },
-		Vertex { { -1.5f, -1.0f, 0.0f}, { 1, 0, 0 } },
-
-		//Triangle1
-		Vertex { { 0.0f, 4.0f, 2.0f }, { 1, 0, 0 } },
-		Vertex { { 3.0f, -2.0f, 2.0f }, { 0, 1, 0 } },
-		Vertex { { -3.0f, -2.0f, 2.0f}, { 0, 0, 1 } }
-	};
-
-	std::vector<Vertex> vetrices_NDC{};
-
-	VertexTransformationFunction(vetrices_world, vetrices_NDC);
-
-	RasterizeTriangle(vetrices_NDC);
-}
-void Renderer::Render_W1_Part5() const
-{
-	const std::vector<Vertex> vetrices_world
-	{
-		//Triangle 0
-		Vertex { { 0.0f, 2.0f, 0.0f }, { 1, 0, 0 } },
-		Vertex { { 1.5f, -1.0f, 0.0f }, { 1, 0, 0 } },
-		Vertex { { -1.5f, -1.0f, 0.0f}, { 1, 0, 0 } },
-
-		//Triangle1
-		Vertex { { 0.0f, 4.0f, 2.0f }, { 1, 0, 0 } },
-		Vertex { { 3.0f, -2.0f, 2.0f }, { 0, 1, 0 } },
-		Vertex { { -3.0f, -2.0f, 2.0f}, { 0, 0, 1 } }
-	};
-
-	std::vector<Vertex> vetrices_NDC{};
-
-	VertexTransformationFunction(vetrices_world, vetrices_NDC);
-
-	RasterizeTriangle(vetrices_NDC);
-}
-void Renderer::Render_W2_Part1() const
-{
-	//Define Triangle List
-	std::vector<Mesh> meshesWorld
-	{
-		Mesh
-		{
-			{
-				Vertex{ { -3, 3, -2 },	{1,1,1} },
-				Vertex{ { 0, 3, -2 },	{1,1,1} },
-				Vertex{ { 3, 3, -2 },	{1,1,1} },
-				Vertex{ { -3, 0, -2 },	{1,1,1} },
-				Vertex{ { 0, 0, -2 },	{1,1,1} },
-				Vertex{ { 3, 0, -2 },	{1,1,1} },
-				Vertex{ { -3, -3, -2 },	{1,1,1} },
-				Vertex{ { 0, -3, -2 },	{1,1,1} },
-				Vertex{ { 3, -3, -2 },	{1,1,1} },
-			},
-
-			{
-				3,0,1,	1,4,3,	4,1,2,
-				2,5,4,	6,3,4,	4,7,6,
-				7,4,5,	5,8,7
-			},
-
-			PrimitiveTopology::TriangleList
-		}
-	};
-
-	std::vector<Vertex> vetrices_world{MeshToVetrices(meshesWorld[0])};
-	std::vector<Vertex> vetrices_NDC{};
-
-	VertexTransformationFunction(vetrices_world, vetrices_NDC);
-	RasterizeTriangle(vetrices_NDC);
-}
-void Renderer::Render_W2_Part2() const
-{
-	//Define Triangle List
-	std::vector<Mesh> meshesWorld
-	{
-		Mesh
-		{
-			{
-				Vertex{ { -3, 3, -2 },	{1,1,1} },
-				Vertex{ { 0, 3, -2 },	{1,1,1} },
-				Vertex{ { 3, 3, -2 },	{1,1,1} },
-				Vertex{ { -3, 0, -2 },	{1,1,1} },
-				Vertex{ { 0, 0, -2 },	{1,1,1} },
-				Vertex{ { 3, 0, -2 },	{1,1,1} },
-				Vertex{ { -3, -3, -2 },	{1,0,1} },
-				Vertex{ { 0, -3, -2 },	{1,1,1} },
-				Vertex{ { 3, -3, -2 },	{1,1,1} },
-			},
-
-			{
-				3,0,4,1,5,2,
-				2,6,
-				6,3,7,4,8,5
-			},
-
-			PrimitiveTopology::TriangleStrip
-		}
-	};
-
-	std::vector<Vertex> vetrices_world{MeshToVetrices(meshesWorld[0])};
-	std::vector<Vertex> vetrices_NDC{};
-
-	VertexTransformationFunction(vetrices_world, vetrices_NDC);
-	RasterizeTriangle(vetrices_NDC);
-}
-void Renderer::Render_W2_Part3() const
-{
-	//Define Triangle List
-	std::vector<Mesh> meshesWorld
-	{
-		Mesh
-		{
-			{
-				Vertex{ { -3, 3, -2 },	{1,1,1},	{0,0} },
-				Vertex{ { 0, 3, -2 },	{1,1,1},	{0.5f,0} },
-				Vertex{ { 3, 3, -2 },	{1,1,1},	{1,0} },
-				Vertex{ { -3, 0, -2 },	{1,1,1},	{0,0.5f} },
-				Vertex{ { 0, 0, -2 },	{1,1,1},	{0.5f,0.5f} },
-				Vertex{ { 3, 0, -2 },	{1,1,1},	{1,0.5f} },
-				Vertex{ { -3, -3, -2 },	{1,1,1},	{0,1} },
-				Vertex{ { 0, -3, -2 },	{1,1,1},	{0.5f,1} },
-				Vertex{ { 3, -3, -2 },	{1,1,1},	{1,1} },
-			},
-
-			{
-				3,0,4,1,5,2,
-				2,6,
-				6,3,7,4,8,5
-			},
-
-			PrimitiveTopology::TriangleStrip
-		}
-	};
-
-	std::vector<Vertex> vetrices_world{ MeshToVetrices(meshesWorld[0]) };
-	std::vector<Vertex> vetrices_NDC{};
-
-	VertexTransformationFunction(vetrices_world, vetrices_NDC);
-	RasterizeTriangle(vetrices_NDC);
-}
-
-void Renderer::Render_W3_Part1()
-{
-	//Define Triangle List
-	std::vector<Mesh> meshesWorld
-	{	Mesh
-		{
-			{
-				Vertex{ { -3, 3, -2 },	{1,1,1},	{0,0} },
-				Vertex{ { 0, 3, -2 },	{1,1,1},	{0.5f,0} },
-				Vertex{ { 3, 3, -2 },	{1,1,1},	{1,0} },
-				Vertex{ { -3, 0, -2 },	{1,1,1},	{0,0.5f} },
-				Vertex{ { 0, 0, -2 },	{1,1,1},	{0.5f,0.5f} },
-				Vertex{ { 3, 0, -2 },	{1,1,1},	{1,0.5f} },
-				Vertex{ { -3, -3, -2 },	{1,1,1},	{0,1} },
-				Vertex{ { 0, -3, -2 },	{1,1,1},	{0.5f,1} },
-				Vertex{ { 3, -3, -2 },	{1,1,1},	{1,1} },
-			},
-
-			{
-				3,0,4,1,5,2,
-				2,6,
-				6,3,7,4,8,5
-			},
-
-			PrimitiveTopology::TriangleStrip
-		}
-	};
-
-	RenderMeshes(meshesWorld);
-}
-void Renderer::Render_W3_Part2()
-{
-	//Define Triangle List
-		//InitMeshes
-	std::vector<Mesh> meshesWorld{};
-
-	std::vector<Vertex> meshVetrices{};
-	std::vector<uint32_t> meshIndices{};
-	Utils::ParseOBJ("Resources/tuktuk.obj", meshVetrices, meshIndices);
-	meshesWorld.push_back(Mesh{ {meshVetrices}, meshIndices, PrimitiveTopology::TriangleList });
-
-	meshesWorld[0].worldMatrix = Matrix::CreateRotationY(m_MeshRotationAngle);
-	RenderMeshes(meshesWorld);
 }
 
 void Renderer::Render_W4_Part1()
--- src/Renderer.h
+++ src/Renderer.h
@@ -44,12 +44,15 @@ namespace dae
 
 		bool SaveBufferToImage() const;
 
-		void VertexTransformationFunction(const std::vector<Vertex>& vertices_in, std::vector<Vertex>& vertices_out) const;
+		//Transform vetrices in world space
 		void VertexTransformationFunction(std::vector<Mesh>& mesh) const;
-		void ToggleDisplayZBuffer();
-		void ToggleRotation();
-		void ToggleNormalMap();
-		void CycleShadingMode();
+
+		//Display functions
+		//Called by input pressure
+		void ToggleDisplayZBuffer(); //F4
+		void ToggleRotation(); //F5
+		void ToggleNormalMap(); //F6
+		void CycleShadingMode(); //F7
 
 	private:
 		SDL_Window* m_pWindow{};
@@ -61,60 +64,53 @@ namespace dae
 
 		Camera m_Camera{ {0,0,0}, 45 };
 
+		//Loaded texture for pixel shading
 		Texture* m_pTexture;
 		Texture* m_pNormalMap;
 		Texture* m_pGlossyMap;
 		Texture* m_pSpecularMap;
-
+		
+		//To calculate the size of the window
 		int m_Width{};
 		int m_Height{};
 
+		//Inpute influenced variables
 		bool m_DisplayZBuffer{ false };
 		bool m_IsRotationEnabled{ false };
 		bool m_DisplayNormalMap{ false };
 		ShadingMode m_ShadingMode{ ShadingMode::ObservedArea };
 
-
+		//Mesh transform
 		float m_MeshRotationAngle{ 0 };
-		const float m_MeshRotationSpeed{ 0.5f };
-		const Vector3 m_MeshPosition{ 0,0,50.0f };
-
-		std::vector<uint32_t> m_PixelIndices{};
+		const float m_MeshRotationSpeed{ 1.0f };
+		const Vector3 m_MeshPosition{ 0,0,0.0f };
 
 
+		//All the meshes
+		std::vector<Mesh> m_MeshesWorld{}; //The vector of mesh on which we apply all the transformation
+		std::vector<Mesh> m_NonTransformedMeshes{}; //The meshes before transformation, so we avoid creating a vector each frame
 
-		std::vector<Mesh> m_MeshesWorld{};
-		std::vector<Mesh> m_NonTransformedMeshes{};
-
+		//Vectors to stors vetrices and indices from ParseOBJ function
 		std::vector<Vertex> m_MeshVetrices{};
 		std::vector<uint32_t> m_MeshIndices{};
 
-
-		bool HitTest_Triangle(const std::vector<Vector2>& triangle, const Vector2& pixel, Vector3& barycentricWeights, const int triangleNr) const;
+		//To check if a triangle could be visible on a certain pixel
 		bool HitTest_Triangle(const Vector2& v0, const Vector2& v1, const Vector2& v2, const Vector2& pixel, Vector3& barycentricWeights) const;
 
-		void RasterizeTriangle(const std::vector<Vertex>& triangle) const;
+		//Render a certain pixel of a mesh
 		void RenderAPixel(const int px, const int py, const Vector4& v0, const Vector4& v1, const Vector4& v2, const Mesh& mesh, const int index);
+
+		//Render all the pixels of a mesh
 		void RenderMeshes(std::vector<Mesh>& meshesWorld);
+
+		//Transform X and Y world value into screenspace values
 		Vector2 ToScreenSpace(const float x, const float y) const;
-		void ClampToNDC(Vector2& point) const;
-		std::vector<Vertex> MeshToVetrices(const Mesh& mesh) const;
 
+		//Remap a value between min and max
 		float Remap(const float colorValue, const float min, const float max) const;
+
+		//Shade a pixel with texture values and Input options
 		ColorRGB PixelShading(const Vertex_Out& vOut) const;
-
-
-		void Render_W1_Part1() const;
-		void Render_W1_Part2() const;
-		void Render_W1_Part3() const;
-		void Render_W1_Part4() const;
-		void Render_W1_Part5() const;
-		void Render_W2_Part1() const; //TriangleList
-		void Render_W2_Part2() const; //TriangleStrip
-		void Render_W2_Part3() const; //UV
-		
-		void Render_W3_Part1(); // Projection Matrix
-		void Render_W3_Part2(); //3D model
 
 		void Render_W4_Part1();
 	};
